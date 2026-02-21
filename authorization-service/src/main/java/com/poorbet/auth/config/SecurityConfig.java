551871578627ef674a28b7dc5233c9d4a4a2c2a3
Dariusz Bosko <dariuszbosko95@gmail.com>
2026-02-21 13:11:17 +0100

    konfiguracja securityFilterChain

--- authorization-service/src/main/java/com/poorbet/auth/config/SecurityConfig.java
+++ authorization-service/src/main/java/com/poorbet/auth/config/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.poorbet.auth.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -18,15 +21,34 @@ public class SecurityConfig {
     private final CorsProperties corsProperties;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                OAuth2AuthorizationServerConfigurer.authorizationServer();
+
+        http
+                .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, authorizationServer ->
+                        authorizationServer.oidc(Customizer.withDefaults())
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/oauth2/**"))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/.well-known/openid-configuration").permitAll()
                         .anyRequest().authenticated()
-                );
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+                )
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
